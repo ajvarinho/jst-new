05a3218bf3652adab3389a6059aef1dd3270c3bf
aywar2000 <polamekkartnija@gmail.com>
2023-12-25 01:25:51 +0100

    deployment test

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import "./style.css";
-import * as THREE from "three";
+// import "./style.css";
+// import * as THREE from "three";
 //import * as Gallery from './components/gallery-animation.js'
 
 const paintings = [
