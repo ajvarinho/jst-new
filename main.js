6aee667e915e87eea68c8d0ccfcf2294826a3a27
aywar2000 <polamekkartnija@gmail.com>
2024-01-03 11:33:30 +0100

    canvas test

--- main.js
+++ main.js
@@ -183,3 +183,55 @@ figureElArr.forEach((image, index) => {
     test.querySelector(".preview-info").style.opacity = 0;
   });
 });
+
+//canvas animation
+
+const testImg = new Image();
+function init() {
+  testImg.src = "./img/paintings/rnd.png";
+  window.requestAnimationFrame(draw);
+}
+
+function draw() {
+  const ctx = document.getElementById("canvas").getContext("2d");
+
+  ctx.globalCompositeOperation = "destination-over";
+  ctx.clearRect(0, 0, 300, 300); // clear canvas
+
+  ctx.fillStyle = "rgba(0, 0, 0, 0.4)";
+  ctx.strokeStyle = "rgba(0, 153, 255, 0.4)";
+  ctx.save();
+  ctx.translate(150, 150);
+
+  // Earth
+  const time = new Date();
+  ctx.rotate(
+    ((2 * Math.PI) / 60) * time.getSeconds() +
+      ((2 * Math.PI) / 60000) * time.getMilliseconds()
+  );
+  ctx.translate(105, 0);
+  ctx.fillRect(0, -12, 40, 24); // Shadow
+  ctx.drawImage(testImg, -12, -12);
+
+  // Moon
+  ctx.save();
+  ctx.rotate(
+    ((2 * Math.PI) / 6) * time.getSeconds() +
+      ((2 * Math.PI) / 6000) * time.getMilliseconds()
+  );
+  ctx.translate(0, 28.5);
+  ctx.drawImage(testImg, 3.5, 3.5);
+  ctx.restore();
+
+  ctx.restore();
+
+  ctx.beginPath();
+  ctx.arc(150, 150, 105, 0, Math.PI * 2, false); // Earth orbit
+  ctx.stroke();
+
+  ctx.drawImage(testImg, 0, 0, 100, 100);
+
+  window.requestAnimationFrame(draw);
+}
+
+init();
