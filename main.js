5a0aa38a222e751978fe993159e0d9a219668788
aywar2000 <polamekkartnija@gmail.com>
2024-03-17 22:05:05 +0100

    exp1

--- main.js
+++ main.js
@@ -1,230 +1,176 @@
-const paintings = [
-  {
-    src: "./public/img/paintings/9.jpg",
-    alt: "",
-    title: "test jedan",
-    info: "",
-  },
+// texture for the particle
+let particle_texture = null;
 
-  {
-    src: "./public/img/paintings/3.jpg",
-    alt: "",
-    title: "test ",
-    info: "",
-  },
-  { src: "./public/img/paintings/4.jpg", alt: "", title: "sfax", info: "" },
-  {
-    src: "./public/img/paintings/5.jpg",
-    alt: "",
-    title: "test",
-    info: "",
-  },
-  {
-    src: "./public/img/paintings/6.jpg",
-    alt: "",
-    title: "test jedan",
-    info: "",
-  },
-  {
-    src: "/public/img/paintings/7.jpg",
-    alt: "",
-    title: "test jedan",
-    info: "",
-  },
-];
+// variable holding our particle system
+let ps = null;
 
-const design = [
-  { src: "./public/img/design/7.jpg", alt: "", title: "", info: "" },
-  { src: "/jst-new/public/img/design/1.jpg", alt: "", title: "", info: "" },
-  { src: "/jst-new/public/img/design/2.jpg", alt: "", title: "", info: "" },
-  { src: "/jst-new/public/img/design/3.jpg", alt: "", title: "", info: "" },
-  { src: "/jst-new/public/img/design/4.jpg", alt: "", title: "", info: "" },
-  { src: "/jst-new/public/img/design/5.jpg", alt: "", title: "", info: "" },
-  { src: "./public/img/design/6.jpg", alt: "", title: "", info: "" },
-  { src: "./public/img/design/7.jpg", alt: "", title: "", info: "" },
-];
-
-const headerBtn = document.getElementById("menu-btn");
-const headerMenu = document.getElementById("menu-list");
-const sectionArr = [...document.querySelectorAll("section")];
-const imgIntro = document.querySelector(".img-intro");
-const aboutBtn = document.getElementById("aboutBtn");
-const aboutWrap = document.getElementById("aboutWrap");
-const worksBtn = document.getElementById("worksBtn");
-const worksWrap = document.getElementById("works");
-const menuWrap = document.getElementById("menuWrap");
-const backBtn = document.getElementById("backBtn");
-const contactBtn = document.getElementById("contactBtn");
-const contactWrap = document.querySelector(".contact-wrap");
-const contactWrapList = document.querySelector(".contact-wrap__list");
-const overlayTitle = document.getElementById("overlay-title");
-
-//
-const welcomeBtn = document.getElementById("welcomeBtn");
-const welcomeEl = document.querySelector(".cover");
-
-// GRID ITEMS
-
-const gridItemOne = document.querySelector(".grid-item.one");
-const gridItemTwo = document.querySelector(".grid-item.two");
-const gridItemThree = document.querySelector(".grid-item.contact-wrap");
-const gridItemFour = document.querySelector(".grid-item.four");
-
-//
-const previewImages = [...document.querySelectorAll(".item__img")];
-const imgPreviewEl = document.querySelector("#active-img");
-
-console.log(imgPreviewEl);
-
-// works navigation
-
-const menuBtn = document.getElementById("nav-works");
-const activeModule = document.getElementById("active-module");
-
-const paintingsBtn = document.getElementById("nav_painting");
-const sculptureBtn = document.getElementById("nav_sculpture");
-const exhibitionsBtn = document.getElementById("nav_exhibition");
-const designBtn = document.getElementById("nav_design");
-
-//
-const closeActiveImg = document.getElementById("close-active");
-const displayActiveImg = document.querySelector(".display");
-
-const previewInfoEl = [...document.querySelectorAll(".preview-info")];
-
-const aboutTitle = document.querySelector(".about-wrap__title");
-const aboutImg = document.getElementById("justyna-img");
-
-// set mobile grid to preview
-const galleryEl = document.getElementById("gallery");
-
-function getSize() {
-  console.log("aloo");
-  let myWidth = 0,
-    myHeight = 0;
-  if (typeof window.innerWidth == "number") {
-    //Non-IE
-    //http://www.howtocreate.co.uk/tutorials/javascript/browserwindow
-    myWidth = window.innerWidth;
-    myHeight = window.innerHeight;
-    console.log(myHeight, myWidth);
-  }
-  if (myWidth <= 500) {
-    console.log(galleryEl, "gallery");
-    galleryEl.classList.add("mobile");
-  }
+function preload() {
+  particle_texture = loadImage("./public/img/paintings/3.jpg");
 }
 
-getSize();
+function setup() {
+  //set the canvas size
+  let canvas = createCanvas(window.innerWidth / 2, window.innerHeight / 2);
+  canvas.parent("#canvas-wrap");
 
-// ANIMACIJE INTRO
+  //initialize our particle system
+  ps = new ParticleSystem(
+    0,
+    createVector(width / 2, height - 60),
+    particle_texture
+  );
+}
 
-const aboutClose = document.getElementById("about-close");
+function draw() {
+  background(255);
 
-welcomeBtn.addEventListener("click", () => {
-  welcomeBtn.classList.add("no-display");
-  welcomeEl.classList.add("anim-opening");
-  menuWrap.classList.remove("start-wide");
-  contactWrap.classList.add("wrap-on");
-});
+  let dx = map(mouseX, 0, width, -0.2, 0.2);
+  let wind = createVector(dx, 0);
 
-aboutBtn.addEventListener("click", () => {
-  aboutWrap.classList.remove("about-wrap");
-  aboutWrap.classList.add("about-wrap-test__open");
-});
+  ps.applyForce(wind);
+  ps.run();
+  for (let i = 0; i < 2; i++) {
+    ps.addParticle();
+  }
 
-aboutClose.addEventListener("click", () => {
-  aboutWrap.classList.remove("about-wrap-test__open");
-  aboutWrap.classList.add("about-wrap");
-});
-
-aboutTitle.addEventListener("click", () => {
-  aboutImg.classList.toggle("active");
-});
-
-worksBtn.addEventListener("click", () => {
-  menuWrap.classList.add("no-height");
-});
-
-backBtn.addEventListener("click", () => {
-  menuWrap.classList.remove("no-height");
-});
-
-contactBtn.addEventListener("click", () => {
-  contactWrapList.classList.toggle("visible");
-});
-
-//WORKS WRAP FUNCTIONALITY
+  // Draw an arrow representing the wind force
+  drawVector(wind, createVector(width / 2, 50, 0), 500);
+}
 
 /**
- * change image sources in preview element
+ *  This function draws an arrow showing the direction our "wind" is blowing.
  */
+function drawVector(v, loc, scale) {
+  push();
+  let arrowsize = 4;
+  translate(loc.x, loc.y);
+  stroke(255);
+  rotate(v.heading());
 
-const test = (array) => {
-  console.log("test fn");
-  const arrSrc = [];
-  array.forEach((source) => {
-    arrSrc.push(source.src);
-  });
-  previewImages.forEach((element, index) => {
-    element.src = arrSrc[index];
-  });
+  let len = v.mag() * scale;
+  line(0, 0, len, 0);
+  line(len, 0, len - arrowsize, +arrowsize / 2);
+  line(len, 0, len - arrowsize, -arrowsize / 2);
+  pop();
+}
+//========= PARTICLE SYSTEM ===========
+
+/**
+ * A basic particle system class
+ * @param num the number of particles
+ * @param v the origin of the particle system
+ * @param img_ a texture for each particle in the system
+ * @constructor
+ */
+let ParticleSystem = function (num, v, img_) {
+  this.particles = [];
+  this.origin = v.copy(); // we make sure to copy the vector value in case we accidentally mutate the original by accident
+  this.img = img_;
+  for (let i = 0; i < num; ++i) {
+    this.particles.push(new Particle(this.origin, this.img));
+  }
 };
 
-paintingsBtn.addEventListener("click", () => {
-  activeModule.innerText = "PAINTINGS";
-  test(paintings);
-});
+/**
+ * This function runs the entire particle system.
+ */
+ParticleSystem.prototype.run = function () {
+  // cache length of the array we're going to loop into a variable
+  // You may see <variable>.length in a for loop, from time to time but
+  // we cache it here because otherwise the length is re-calculated for each iteration of a loop
+  let len = this.particles.length;
 
-sculptureBtn.addEventListener("click", () => {
-  activeModule.innerText = "SCULPTURE";
-  test(design);
-});
+  //loop through and run particles
+  for (let i = len - 1; i >= 0; i--) {
+    let particle = this.particles[i];
+    particle.run();
 
-exhibitionsBtn.addEventListener("click", () => {
-  activeModule.innerText = "EXXHIBITIONS";
-});
+    // if the particle is dead, we remove it.
+    // javascript arrays don't have a "remove" function but "splice" works just as well.
+    // we feed it an index to start at, then how many numbers from that point to remove.
+    if (particle.isDead()) {
+      this.particles.splice(i, 1);
+    }
+  }
+};
 
-designBtn.addEventListener("click", () => {
-  activeModule.innerText = "DESIGN";
-});
+/**
+ * Method to add a force vector to all particles currently in the system
+ * @param dir a p5.Vector describing the direction of the force.
+ */
+ParticleSystem.prototype.applyForce = function (dir) {
+  let len = this.particles.length;
+  for (let i = 0; i < len; ++i) {
+    this.particles[i].applyForce(dir);
+  }
+};
 
-previewImages.forEach((image, index) => {
-  image.addEventListener("click", (event) => {
-    console.log("c;olick");
-    let test = event.target;
-    let imageSrc = image.src;
-    console.log("test src", imageSrc);
-    imgPreviewEl.src = imageSrc;
-    overlayTitle.innerText = paintings[index].title;
-    displayActiveImg.classList.remove("hide");
-    displayActiveImg.classList.add("show");
-  });
-});
+/**
+ * Adds a new particle to the system at the origin of the system and with
+ * the originally set texture.
+ */
+ParticleSystem.prototype.addParticle = function () {
+  this.particles.push(new Particle(this.origin, this.img));
+};
 
-previewImages.forEach((image, index) => {
-  image.addEventListener("click", (event) => {
-    console.log("a;loooooooooooooooo");
-    displayActiveImg.classList.remove("hide");
-    displayActiveImg.classList.add("show");
-  });
-});
+//========= PARTICLE  ===========
+/**
+ *  A simple Particle class, renders the particle as an image
+ */
+let Particle = function (pos, img_) {
+  this.loc = pos.copy();
 
-const figureElArr = [...document.querySelectorAll("figure")];
+  let vx = randomGaussian() * 0.6;
+  let vy = randomGaussian() * 0.3 - 1.0;
 
-figureElArr.forEach((image, index) => {
-  image.addEventListener("click", (event) => {
-    let test = event.target;
-    //test.querySelector(".preview-info").innerText = paintings[index].title;
-    test.querySelector(".preview-info").style.opacity = 1;
-  });
-  // image.addEventListener("mouseleave", (event) => {
-  //   let test = event.target;
-  //   test.querySelector(".preview-info").style.opacity = 0;
-  // });
-});
+  this.vel = createVector(vx, vy);
+  this.acc = createVector();
+  this.lifespan = 200.0;
+  this.texture = img_;
+};
 
-closeActiveImg.addEventListener("click", () => {
-  displayActiveImg.classList.remove("show");
-  displayActiveImg.classList.add("hide");
-});
+/**
+ *  Simulataneously updates and displays a particle.
+ */
+Particle.prototype.run = function () {
+  this.update();
+  this.render();
+};
+
+/**
+ *  A function to display a particle
+ */
+Particle.prototype.render = function () {
+  imageMode(CENTER);
+  tint(255, this.lifespan);
+  image(this.texture, this.loc.x, this.loc.y);
+};
+
+/**
+ *  A method to apply a force vector to a particle.
+ */
+Particle.prototype.applyForce = function (f) {
+  this.acc.add(f);
+};
+
+/**
+ *  This method checks to see if the particle has reached the end of it's lifespan,
+ *  if it has, return true, otherwise return false.
+ */
+Particle.prototype.isDead = function () {
+  if (this.lifespan <= 0.0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+/**
+ *  This method updates the position of the particle.
+ */
+Particle.prototype.update = function () {
+  this.vel.add(this.acc);
+  this.loc.add(this.vel);
+  this.lifespan -= 2.5;
+  this.acc.mult(0);
+};
