3fe195aa15b151472d5db9c6bd53bf4623423f3f
aywar2000 <polamekkartnija@gmail.com>
2024-03-28 17:21:59 +0100

    adding vals

--- main.js
+++ main.js
@@ -17,23 +17,93 @@ console.log("b4 re-sizin", introHeight, introWidth);
 
 //
 let init_x;
+let init_y;
+let init_rad;
 let new_x;
+let new_y;
+let new_rad;
+
+/**
+ * test - how to handle window resizing
+ */
+
+function anotherFunction(numericValue) {
+  console.log("Received numeric value:", numericValue);
+  // Do something with the numeric value here
+}
+
+// Function to handle the resize event
+function handleResize() {
+  // Retrieve the numeric value (e.g., window's inner width)
+  var numericValue = window.innerWidth;
+  // Pass the numeric value to another function
+  anotherFunction(numericValue);
+}
+
+// Attach the resize event listener to the window object
+//window.addEventListener("resize", handleResize);
+
+// Optionally, call the handleResize function initially to handle the current window size
+//handleResize();
+
+/**
+ * init values - moram li za svaki eleemnt sve?
+ */
+
+let values = {
+  ellipseOne: [innerWidth / 2.5 + 100, 270, 380, 380],
+  ellipseTwo: [innerWidth / 2.5 + 50, 270, 380, 380],
+  elipseThree: [innerWidth / 2.5 - 50, 270, 380, 380],
+  ellipseFour: [innerWidth / 2.5 - 100, 250, 380, 380],
+  ellipseFive: [innerWidth / 2.5 - 130, 220, 380, 380],
+  ellipseSix: [innerWidth / 2.5 - 150, 190, 380, 380],
+  ellipseSeven: [innerWidth / 2.5 - 70, 150, 380, 380],
+};
+
+let rectOne = [innerWidth - (fractionWidth + 220), 350, 250, 300];
+let rectTwo = [];
+let rectThree = [];
+let rectFour = [];
+let rectFive = [];
+let rectSix = [];
+let rectSeven = [];
+let rectEight = [];
+let rectNine = [];
+
+//
+let rectSmallOne = [];
+let rectSmallTwo = [];
+let rectSmallThree = [];
+let rectSmallFour = [];
+let rectSmallFive = [];
 
 /**
  * staviti listener unutar funkcije
  * funkcija vraca newWidth i percentage
  */
 
-const resized = window.addEventListener("resize", () => {
+function handleCanvasResizing() {
   newWidth = introWrap.offsetWidth;
   newHeight = introWrap.offsetHeight;
   init_x = Math.round(innerWidth / 2.5 + 100);
+  init_y = 270;
+  init_rad = 380;
   percentageX = Math.round((newWidth / introWidth) * 100);
   percentageY = Math.round((newHeight / introHeight) * 100);
   percentageChangeX = 100 - percentageX;
   percentageChangeY = 100 - percentageY;
-  new_x = Math.round(init_x - (percentageChangeX / 100) * init_x);
-  draw(new_x, percentageChangeX, percentageChangeY);
+  values.ellipseOne[0] = Math.round(
+    init_x - (percentageChangeX / 100) * init_x
+  );
+  new_y = Math.round(init_y - (percentageChangeY / 100) * init_y);
+  new_rad = Math.round(init_rad - (percentageChangeX / 100) * init_rad);
+  draw();
+}
+
+const resized = window.addEventListener("resize", handleCanvasResizing);
+
+window.addEventListener("load", () => {
+  handleCanvasResizing();
 });
 
 let pg;
@@ -63,14 +133,12 @@ function setup() {
   canvas.parent("#canvas-wrap");
 }
 
-function draw(new_x, percentageChangeX, percentageChangeY) {
-  //Math.round(innerWidth / 2.5 + 100)
-  //const init_x =
-  let init_y = 270;
-  let init_rad = 380;
-  //
-  console.log("drawin ^ resizin for %", percentageChangeX, percentageChangeY);
-  console.log("rect x initial", init_x);
+console.log(values, typeof values);
+
+function draw() {
+  let check = values;
+  console.log(check);
+
   background(255, 255, 255);
   fill(200);
   //
@@ -78,23 +146,30 @@ function draw(new_x, percentageChangeX, percentageChangeY) {
   strokeWeight(1);
   stroke("blue");
   ellipseMode(CORNER);
-  //
-  //ellipse(new_x, 270, 380, 380);
-  ellipse(innerWidth / 2.5 + 100, 270, 380, 380);
-  ellipse(innerWidth / 2.5 + 50, 270, 380, 380);
+
+  console.log("got vals: ", new_x, new_y, new_rad);
+
+  ellipse(
+    values.ellipseOne[0],
+    values.ellipseOne[1],
+    values.ellipseOne[2],
+    values.ellipseOne[3]
+  );
+  // ellipse(innerWidth / 2.5 + 100, 270, 380, 380);
+  // ellipse(innerWidth / 2.5 + 50, 270, 380, 380);
+  // // //
+  // ellipse(innerWidth / 2.5 - 50, 270, 380, 380);
+  // ellipse(innerWidth / 2.5 - 100, 250, 380, 380);
+  // ellipse(innerWidth / 2.5 - 130, 220, 380, 380);
+  // ellipse(innerWidth / 2.5 - 150, 190, 380, 380);
   // //
-  ellipse(innerWidth / 2.5 - 50, 270, 380, 380);
-  ellipse(innerWidth / 2.5 - 100, 250, 380, 380);
-  ellipse(innerWidth / 2.5 - 130, 220, 380, 380);
-  ellipse(innerWidth / 2.5 - 150, 190, 380, 380);
-  //
-  ellipse(innerWidth / 2.5 - 70, 150, 380, 380);
+  // ellipse(innerWidth / 2.5 - 70, 150, 380, 380);
 
   strokeWeight(1);
   stroke("blue");
   //border
-  ellipse(innerWidth / 2.5 - 50, 100, 382, 382);
-  image(imgOne, innerWidth / 2.5 - 50, 100, 380, 380);
+  //ellipse(innerWidth / 2.5 - 50, 100, 382, 382);
+  //image(imgOne, innerWidth / 2.5 - 50, 100, 380, 380);
 
   //
   strokeWeight(1);
